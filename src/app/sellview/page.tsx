8071700766b0323c15ce31882817d1f1a114cce6
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-05-07 00:18:22 -0600

    codebase documentation -sellview/page-

--- src/app/sellview/page.tsx
+++ src/app/sellview/page.tsx
@@ -1,9 +1,10 @@
+//Necessary imports
 import { Inventory } from './inventory';
-
-
 import NavbarR from '../componentes/navbarR';
 
+// Define functional component Page
 const Page = () => {
+  // Return JSX for rendering the page
   return (
     <div className="bg-backgroundBlue min-h-screen w-screen flex flex-col mx-auto p-0 overflow-y-auto">
       <NavbarR />
