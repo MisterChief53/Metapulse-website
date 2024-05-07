99c287e45bed5d3e686e9a87af90fd1116fc4f5e
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-05-07 00:20:48 -0600

    codebase documentation -sellview/itemList

--- src/app/sellview/itemsList.tsx
+++ src/app/sellview/itemsList.tsx
@@ -1,5 +1,6 @@
 import { ButtonInventory } from './buttonInventory';
 
+// Define interface for user item
 interface UserItem {
   id: number;
   name: string;
@@ -11,25 +12,29 @@ interface UserItem {
   tradableStatus: boolean;
 }
 
+// Define interface for ItemsListProps
 interface ItemsListProps {
   userItems: UserItem[];
 }
 
+// Define functional component ItemsList
 const ItemsList = ({
   itemsList,
   setItemDetails,
 }: {
   itemsList: UserItem[];
-  setItemDetails: React.Dispatch<React.SetStateAction<any>>;
+  setItemDetails: React.Dispatch<React.SetStateAction<any>>;// Function to set item details
 }) => {
+  // Return JSX for rendering the list of user items
   return (
     <>
+    {/* Map through user items and render ButtonInventory component for each item */}
       {itemsList.map((item) => {
         return (
           <ButtonInventory
-            key={item.id}
-            item={item}
-            setItemDetails={setItemDetails}
+            key={item.id} // Unique key for each item
+            item={item} // Item details
+            setItemDetails={setItemDetails} // Function to set item details
           />
         );
       })}
