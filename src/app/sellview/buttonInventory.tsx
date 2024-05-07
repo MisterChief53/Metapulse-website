bb63d61d575930ec1694eaad224329796e812a84
RitaMonreal <A326347@alumnos.uaslp.mx>
2024-05-07 00:09:08 -0600

    codebase documentation -sellview/buttonInventory-

--- src/app/sellview/buttonInventory.tsx
+++ src/app/sellview/buttonInventory.tsx
@@ -1,4 +1,6 @@
+// Define and export functional component ButtonInventory
 export const ButtonInventory = ({ item, setItemDetails }: { item: any; setItemDetails: Function }) => {
+  // Return JSX for rendering the button
   return (
     <button
       className=" border-black border-2 rounded-md w-10/12 mx-auto py-2 flex justify-start mt-4 hover:bg-slate-500"
@@ -7,6 +9,7 @@ export const ButtonInventory = ({ item, setItemDetails }: { item: any; setItemDe
         setItemDetails(item);
       }}
     >
+      {/* Render item name */}
       <span className="font-bold text-xl text-left w-full ml-5">
         {item.name}
       </span>
