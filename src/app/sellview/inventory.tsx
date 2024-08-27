cf402ab9e61fc9c7f6cdc44078ed463b969b7574
Rita Monreal García <98373365+RitaMonreal@users.noreply.github.com>
2024-08-27 17:19:44 -0600

    Update inventory.tsx

--- src/app/sellview/inventory.tsx
+++ src/app/sellview/inventory.tsx
@@ -28,7 +28,7 @@ export const Inventory = () => {
       try {
         let formData = new FormData();
         formData.append('name', name);
-        const res = await fetch('http://localhost:8080/items/getItemsUser', {
+        const res = await fetch('http://10.103.160.60:8080/items/getItemsUser', {
           method: 'post',
           body: formData,
           cache: 'no-cache',
@@ -56,7 +56,7 @@ export const Inventory = () => {
           if (token) {
             // Fetch user info from the server
             const response = await fetch(
-              'http://localhost:8080/auth/userInfo',
+              'http://10.103.160.60:8080/auth/userInfo',
               {
                 headers: {
                   Authorization: token,
