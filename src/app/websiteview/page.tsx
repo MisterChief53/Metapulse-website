3ab62d6745b0c87e670f5332ac85d0fd8d3dd463
Rita Monreal García <98373365+RitaMonreal@users.noreply.github.com>
2024-09-05 16:35:02 -0600

    Update page.tsx
    Environmental variables

--- src/app/websiteview/page.tsx
+++ src/app/websiteview/page.tsx
@@ -24,7 +24,7 @@ function Page() {
     const intervalId = setInterval(async () => {
       // Fetch items from the server
       try {
-        const res = await fetch(`http://10.103.160.60:8080/sales/items`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/sales/items`, {
           method: 'GET',
           headers: {
             'Cache-Control': 'no-cache', // Request not to cache in browser
