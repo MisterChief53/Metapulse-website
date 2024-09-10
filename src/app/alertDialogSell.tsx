34ade646ffbb9b934a8154e1ae4066475c120356
Rita Monreal García <98373365+RitaMonreal@users.noreply.github.com>
2024-09-10 17:37:59 -0600

    Update alertDialogSell.tsx

--- src/app/alertDialogSell.tsx
+++ src/app/alertDialogSell.tsx
@@ -57,7 +57,7 @@ export async function sellItem({
     };
 
     const itemResult = await fetch(
-       `${process.env.NEXT_PUBLIC_API_BASE_URL}/sales/items`,
+       `${process.env.NEXT_PUBLIC_API_BASE_URL}:8080/sales/items`,
       reqOptions
     );
 
@@ -88,7 +88,7 @@ export function AlertDialogSell({ buttonText, itemDetails }: { buttonText: strin
 
   const returnToMenu = () => {
     setModalExitoAbierto(false);
-    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}/websiteview`;
+    window.location.href = `${process.env.NEXT_PUBLIC_BASE_URL}:3000/websiteview`;
   };
   return (
     <>
