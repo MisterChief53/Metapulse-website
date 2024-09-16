affef2717ed83f3d8aeadd1ff9cc1be535d4093b
Rita Monreal García <98373365+RitaMonreal@users.noreply.github.com>
2024-09-16 14:05:27 -0600

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reactStrictMode: true,
     proxy: {
       '/api': {
-        target: NEXT_PUBLIC_API_BASE_URL,
+        target: `${process.env.NEXT_PUBLIC_API_BASE_URL}:8080`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
